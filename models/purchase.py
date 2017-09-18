d1bfec2357284c9efa478cd4f399a3bba3a39553
Ruben Dario Bravo <rbravo@hoatzin.org>
2017-09-18 12:56:50 -0400

    [ADD] MODULO QUE PERMITE REALIZAR DOBLE VALIDACION PARA UNA ORDEN DE COMPRA BASADO EN CANTIDAD DE PRODUCTOS

---
+++ models/purchase.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2017 Humanytek (<www.humanytek.com>).
+#    Manuel Márquez <manuel@humanytek.com>
+#    Rubén Bravo <rubenred18@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+
+from openerp import fields, api, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+    _name = "purchase.order"
+
+    @api.depends('order_line.product_qty')
+    def _compute_qty(self):
+        for order in self:
+            qty_total = 0.0
+            for line in order.order_line:
+                qty_total += line.product_qty
+            order.update({
+                'product_qty_total': qty_total,
+            })
+
+    product_qty_total = fields.Float(compute='_compute_qty',
+                                    string='Cantidad total de productos',
+                                    default=0)
+
+    @api.multi
+    def button_confirm(self):
+        for order in self:
+            if order.state not in ['draft', 'sent']:
+                continue
+            order._add_supplier_to_product()
+            # Deal with double validation process
+            #if (order.company_id.po_double_validation == 'one_step'\ and
+                    #order.company_id.po_double_validation_product == 'one_step')
+                    #or (order.company_id.po_double_validation == 'two_step'\
+                        #and order.amount_total < self.env.user.company_id.currency_id.compute(order.company_id.po_double_validation_amount, order.currency_id))\
+                    #or order.user_has_groups('purchase.group_purchase_manager'):
+                #order.button_approve()
+            #else:
+                #order.write({'state': 'to approve'})
+
+            if ((order.company_id.po_double_validation == 'two_step'\
+                        and order.amount_total >= self.env.user.company_id.currency_id.compute(order.company_id.po_double_validation_amount, order.currency_id))\
+                    or (order.company_id.po_double_validation_product == 'two_step'\
+                        and order.product_qty_total >= self.env.user.company_id.po_double_validation_product_qty)) \
+                    and not order.user_has_groups('purchase.group_purchase_manager'):
+                order.write({'state': 'to approve'})
+            else:
+                order.button_approve()
+
+        return {}
